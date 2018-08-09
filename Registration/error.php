83549e3cd9e6b5d5f2a7966af6ec20d57682d3c4
Amal <Amal.Salah@souktel.org>
2018-08-09 16:20:27 -0700

    dsad

--- Registration/error.php
+++ Registration/error.php
@@ -4,11 +4,12 @@
  * User: Amal
  * Date: 8/7/2018
  * Time: 4:18 PM
+ *Edit
  */
 if(count($errors) >0 ): ?>
 
 <?php foreach($errors as $error): ?>
-<p> <?php echo "test".$error; ?> </p>
-<?php endforeach ?>
-<?php endif ?>
+<p> <?php echo $error; ?> </p>
+<?php endforeach;
+endif ?>
 
